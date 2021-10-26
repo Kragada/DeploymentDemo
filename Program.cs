897e08baf0129b33d6c7ddc09cef635f512c5f89
Kragada <35291972+Kragada@users.noreply.github.com>
2021-10-26 23:30:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DeploymentDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World2");
             Console.WriteLine("Hello World once more!");
         }
     }
